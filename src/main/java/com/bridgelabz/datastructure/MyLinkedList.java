6392df20ef55f6135a96b5140d04689a559c342f
namo-patil <namratapatil74738@gmail.com>
2022-06-12 20:17:58 +0530

    Added: Nodes to the Linked List at Front

---
+++ src/main/java/com/bridgelabz/datastructure/MyLinkedList.java
@@ -0,0 +1,33 @@
+package com.bridgelabz.datastructure;
+
+public class MyLinkedList {
+    public INode head;
+    public INode tail;
+
+    public MyLinkedList() {
+        this.head = null;
+        this.tail = null;
+    }
+
+    public void add(INode newNode) {
+        if (this.tail == null) {
+            this.tail = newNode;
+        }
+        if (this.head == null) {
+            this.head = newNode;
+        } else {
+            INode currNode = head;
+            this.head = newNode;
+            this.head.setNext(currNode);
+        }
+    }
+
+    public void printMyNode() {
+        INode currNode = head;
+        while (currNode != null) {
+            System.out.print(currNode.getKey() + "-->");
+            currNode = currNode.getNext();
+        }
+        System.out.println("null");
+    }
+}
